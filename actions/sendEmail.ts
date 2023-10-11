6118d04346864b8c56447eec9fb05e2059000184
Anwarkamal143 <anwarkamal1434@gmail.com>
2023-10-11 11:31:57 +0500

    updated dev

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form Job",
+      from: senderEmail,
       to: "anwarkamal1434@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
